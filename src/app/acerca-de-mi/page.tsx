d6a38a2b1475dc57b9cbda4ff16d291bbb2bf0e3
Joaquin <joaco.vla@gmail.com>
2024-04-16 00:25:13 -0300

    skill motion

--- src/app/acerca-de-mi/page.tsx
+++ src/app/acerca-de-mi/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Brain from "@/components/Brain";
 import Skills from "@/components/Skills";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useInView } from "framer-motion";
 import {
   SiMongoose,
   SiMongodb,
@@ -20,54 +20,61 @@ import {
   SiAdobephotoshop,
   SiReact,
   SiFramer,
+  SiTrello,
 } from "react-icons/si";
 import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaNodeJs, FaGithub, FaGitAlt } from "react-icons/fa6";
-import { useRef } from "react";
-import React from "react";
+import React, { useRef } from "react";
 
 const About = () => {
   const containerRef = useRef<HTMLElement | null>(null);
+  const skillTitleRef = useRef<HTMLDivElement | null>(null);
+  const skillRef = useRef<HTMLDivElement | null>(null);
+  const experienceRef = useRef<HTMLDivElement | null>(null);
+
   const { scrollYProgress } = useScroll({ container: containerRef });
 
-  console.log(scrollYProgress);
+  const isSkillInView = useInView(skillRef);
+  const isSkillTitleInView = useInView(skillTitleRef);
+  const isExperienceInView = useInView(experienceRef);
 
   const skillsData = [
     {
       title: "Back-end",
       data: [
-        <SiMongodb />,
-        <SiExpress />,
-        <SiMongoose />,
-        <SiPostgresql />,
-        <SiJsonwebtokens />,
-        <SiPassport />,
-        <FaNodeJs />,
+        { icon: <SiMongodb />, name: "MongoDB" },
+        { icon: <SiExpress />, name: "Express" },
+        { icon: <SiMongoose />, name: "Mongoose" },
+        { icon: <SiPostgresql />, name: "PostgreSQL" },
+        { icon: <SiJsonwebtokens />, name: "JSON Web Token" },
+        { icon: <SiPassport />, name: "Passport.js" },
+        { icon: <FaNodeJs />, name: "Node.js" },
       ],
     },
     {
       title: "Front-end",
       data: [
-        <SiTailwindcss />,
-        <SiTypescript />,
-        <SiCss3 />,
-        <SiHtml5 />,
-        <SiJavascript />,
-        <SiReact />,
-        <TbBrandNextjs />,
-        <SiFramer />,
+        { icon: <SiTailwindcss />, name: "Tailwind CSS" },
+        { icon: <SiTypescript />, name: "TypeScript" },
+        { icon: <SiCss3 />, name: "CSS3" },
+        { icon: <SiHtml5 />, name: "HTML5" },
+        { icon: <SiJavascript />, name: "JavaScript" },
+        { icon: <SiReact />, name: "React" },
+        { icon: <TbBrandNextjs />, name: "Next.js" },
+        { icon: <SiFramer />, name: "Framer" },
       ],
     },
     {
       title: "Herramientas",
       data: [
-        <SiFigma />,
-        <SiVisualstudiocode />,
-        <SiAmazonaws />,
-        <SiAdobephotoshop />,
-        <FaGitAlt />,
-        <FaGithub />,
+        { icon: <SiFigma />, name: "Figma" },
+        { icon: <SiVisualstudiocode />, name: "Visual Studio Code" },
+        { icon: <SiAmazonaws />, name: "Amazon Web Services (AWS)" },
+        { icon: <SiAdobephotoshop />, name: "Adobe Photoshop" },
+        { icon: <FaGitAlt />, name: "Git" },
+        { icon: <FaGithub />, name: "GitHub" },
+        { icon: <SiTrello />, name: "Trello" },
       ],
     },
   ];
@@ -95,30 +102,45 @@ const About = () => {
               ingenieria en sistemas.
             </p>
           </div>
-          <div className="hidden pt-16 lg:flex flex-row items-center animate-bounce">
+          <motion.div
+            initial={{ opacity: 0.2, y: 0 }}
+            animate={{ opacity: 1, y: 10 }}
+            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+            className="hidden pt-16 lg:flex flex-row items-center"
+          >
             <span className="mr-1">SKILLS</span>
-            <MdKeyboardDoubleArrowDown className="text-4xl text-light-red" />
-          </div>
+            <div>
+              <MdKeyboardDoubleArrowDown className="text-4xl text-light-red" />
+            </div>
+          </motion.div>
         </div>
         <div className="hidden lg:block w-1/3 sticky top-0 right-0 z-30 xl:w-1/2">
           <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
       <div className="p-4 sm:p-8 md:p-12 lg:px-20 xl:px-48 xl:pt-8 flex flex-col gap-12">
-        <h3 className="font-bold text-3xl">
+        <motion.h3
+          ref={skillTitleRef}
+          initial={{ x: -300 }}
+          animate={isSkillTitleInView ? { x: 0 } : {}}
+          className="font-bold text-3xl"
+        >
           SKILLS<span className="text-light-red">.</span>
-        </h3>
-        <div className="flex flex-col sm:flex-row sm:justify-between gap-4 md:gap-8">
+        </motion.h3>
+        <div
+          ref={skillRef}
+          className="flex flex-col sm:flex-row sm:justify-between gap-4 md:gap-8"
+        >
           {skillsData.map((skill, index) => (
             <React.Fragment key={index}>
-              <Skills title={skill.title} icons={skill.data} />
+              <Skills
+                title={skill.title}
+                skill={skill.data}
+                isSkillInView={isSkillInView}
+              />
             </React.Fragment>
           ))}
         </div>
-        {/* <div className="hidden pt-16 lg:flex flex-row items-center animate-bounce">
-          <span className="mr-1">EXPERIENCIA</span>
-          <MdKeyboardDoubleArrowDown className="text-4xl text-light-red" />
-        </div> */}
       </div>
       <div className="p-4 sm:p-8 md:p-12 lg:px-20 xl:px-48 xl:pt-8 flex flex-col gap-12">
         <h3 className="font-bold text-3xl">
